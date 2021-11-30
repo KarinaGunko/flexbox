183f83335a807e8571d83345da27bd10dc462743
COMPUTER\DELL <gunkokarina987@gmail.com>
2021-11-30 14:49:06 +0200

    add request

---
+++ js/request.js
@@ -0,0 +1,22 @@
+// let xhttp = new XMLHttpRequest();
+
+// xhttp.onreadystatechange = function() {
+// 	if (this.readyState == 4 && this.status == 200) {
+// 		myFunction(this.responseText)
+// 	}
+// }
+
+// xhttp.open("GET","index2.txt", true);
+// xhttp.send();
+
+// function myFunction(button) {
+// 	console.log(button);
+// };
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("demo").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "ndex2.txt");
+  xhttp.send();
+}
