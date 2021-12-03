b23d618271e0301b78ec66653b40db615136d1c2
COMPUTER\DELL <gunkokarina987@gmail.com>
2021-12-03 20:35:14 +0200

    add new

---
+++ js/form-function.js
@@ -0,0 +1,32 @@
+// const x = function (a, b) {return a * b};
+// document.getElementById("demo").innerHTML = x(4, 3); //1
+
+// const  winter = function (f, g) {
+// 	return (f + g);
+// };
+// document.getElementById("demo").innerHTML = winter(10, 33); //2
+
+// function myFunction(a, b) {
+//   return a * b;
+// }
+// console.log(myFunction); //3
+
+// var x = function(x, y) {
+//   return x * y;
+// }
+// document.getElementById("demo").innerHTML = x(12, 56); //4
+
+// const x = (x, y) => x * y;
+//  document.getElementById("demo").innerHTML = x(10, 1000); //5
+
+// function myFunction(a, b) {
+// 	return a * b;
+// }
+// let x = myFunction(4, 5) % 6;
+// document.getElementById("demo").innerHTML = x; //6
+
+// function myFunction(a, b) {
+// 	return arguments.length;
+// }
+// document.getElementById("demo").innerHTML = myFunction(12, 3); //7
+
