b9bda668680db5139651aac6c71c03a2989f5ad4
COMPUTER\DELL <gunkokarina987@gmail.com>
2021-12-05 14:25:49 +0200

    add function

--- js/form-function.js
+++ js/form-function.js
@@ -30,3 +30,108 @@
 // }
 // document.getElementById("demo").innerHTML = myFunction(12, 3); //7
 
+// function string(karina, blue) {
+// 	name: 'karina' ;
+// }
+// console.log('karina');
+
+
+// function getFullName(firstName, lastName) {
+// 	return firstName + ' ' + lastName;
+// }
+
+// const name1 = getFullName('karina', 'Gunko');
+
+// console.log('name1', name1);
+
+// const name2 = getFullName('Vika', 'Petrova');
+
+// console.log('name2', name2);
+
+// const name3 = getFullName('Vasya', 'Ivanov');
+
+// console.log('name3', name3);
+
+
+
+// виклик функциї
+
+// function myFunction(a, b) {
+//   return a * b;
+// }
+// myFunction(10, 2);    
+// document.getElementById("demo").innerHTML = myFunction(10, 2); 
+
+
+// function myFunction(a, b) {
+//   return a * b;
+// }
+// window.myFunction(10, 2);
+// document.getElementById("demo").innerHTML = window.myFunction(10, 2); 
+
+
+
+// let name = myFunction();
+// function myFunction() {
+//   return this;
+// }
+// document.getElementById("demo").innerHTML = name; 
+
+
+// const myObject = {
+//   firstName:"Karina",
+//   lastName: "Gunko",
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+// document.getElementById("demo").innerHTML = myObject.fullName(); 
+
+
+// const myObject = {
+//   firstName:"John",
+//   lastName: "Doe",
+//   fullName: function() {
+//     return this;
+//   }
+// }
+// document.getElementById("demo").innerHTML = myObject.fullName();
+
+
+// function myFunction(arg1, arg2) {
+//   this.firstName = arg1;
+//   this.lastName  = arg2;
+// }
+
+// const myObj = new myFunction("Ivan","Ivanov")
+// document.getElementById("demo").innerHTML = myObj.firstName; 
+
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+//   fullName : function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// };
+// console.log(this);
+
+
+// function myFunction() {
+//   return this;
+// };
+
+
+// const person1 = {
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+// const person2 = {
+//   firstName:"John",
+//   lastName: "Doe",
+// }
+// let one = person1.fullName.call(person2); 
+
+// console.log(one);
